e88e762ba05e5b720c09e173d2ec1481025889f2
Luisisluisis <158091978+Luisisluisis@users.noreply.github.com>
2024-02-11 20:56:46 -0500

    caraosello.cpp

--- caraosello.cpp
+++ caraosello.cpp
@@ -5,27 +5,27 @@
 using namespace std;
 
 int main() {
-    default_random_engine generator(time(0));
-    uniform_int_distribution<int> distribution(1, 2);
+    defaultRandomEngine generator (time(0)); // es un generador de numeros aleatorios usando la hora
+    uniformIntDistribution <int> distribution (1, 2); //genera valores entre 1 y 2
 
     int eleccion;
     char res;
 
     cout << "Choose one: [1] Cara [2] Sello" << endl;
-    cin >> eleccion;
+    cin >> eleccion; // aca se guarda la opcion que tomo el usuario en la variable eleccion
 
     if (eleccion < 1 || eleccion > 2) {
         cout << "Error, try again." << endl;
-        return 0;
+        return 0; // si el usuario elige dentro del rango de 1 o 2 bien pero si no esta en ese rango se ejecuta error y el programa deja de ejecutar
     }
 
-    if (distribution(generator) == 1) {
-        res = (eleccion == 1) ? 'S' : 'C';
+    if (distribution(generator) == 1) { // crea valor aleatorio
+        res = (eleccion == 1) ? 'S' : 'C'; // s para sello y c para cara
     } else {
         res = (eleccion == 1) ? 'C' : 'S';
     }
 
-    cout << "It is: " << res << endl;
+    cout << "It is: " << res << endl; // mensaje de salida con resultado
 
     return 0;
 }
