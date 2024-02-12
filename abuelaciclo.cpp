32688e565a969042462a22ef4d67c6f23f04a500
Luisisluisis <158091978+Luisisluisis@users.noreply.github.com>
2024-02-11 20:12:52 -0500

    abuelaciclo.cpp
    Utilizo un ciclo do while para poder crear un convertidor de farenheit a celsius las veces que el usuario necesite convertir estas temperaturas. Cuando el usuario decide terminar de convertir, se le muestra un ultimo mensaje diciendo que ya se acabo el proceso

--- abuelaciclo.cpp
+++ abuelaciclo.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    char repeat; // repeat va a ser una variable que utilizare para guardar las temperaturas que el usuario haya digitado
+
+    do {
+        float temp, res;
+
+        cout << "Give me the temperature in Fahrenheit: ";
+        cin >> temp;
+
+        res = (temp - 32) / 1.8;
+
+        cout << "The temperature in Celsius is: " << res << endl;
+
+        cout << "Do you want to convert another temperature? (y/n): ";
+        cin >> repeat;
+
+    } while (repeat == 'y' || repeat == 'Y'); // si el usuario quiere seguir convirtiendo las temperaturas se va a repetir el codigo, si no, se acaba el convertidor 
+
+    cout << "End of convertions" << endl; // como ultimo mensaje de salida hacia el usuario se le dice que el programa finalizo
+
+    return 0;
+}
