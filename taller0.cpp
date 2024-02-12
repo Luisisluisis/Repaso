d22dd99f1f5f2d7d1348ae9c07ef2fd27dd6948d
Luisisluisis <158091978+Luisisluisis@users.noreply.github.com>
2024-02-12 10:09:00 -0500

    taller0.cpp

---
+++ taller0.cpp
@@ -0,0 +1,37 @@
+// 1. A fin de recuperar la cartera morosa, a través de la secretaría de hacienda se ha decidido hacer un 
+// descuento a los deudores morosos del impuesto predial para los estratos 1, 2 y 3 de acuerdo con las 
+// siguientes condiciones:
+
+// 2. Para deudas mayores a 60000 pesos y menores a 150000 pesos se da un descuento del 20% para deudas entre 
+// 150000 pesos y 300000 se da un descuento del 30% para deudas mayores a 300000 hasta 800000 se da un 
+// descuento del 40% para deudas mayores a 800000 se da un descuento del 50%. Calcular e imprimir el descuento 
+// a realizar y el valor a pagar con descuento.
+
+
+// 3. Una empresa de telefonía celular tiene esquemas de descuento en sus planes de prepago y postpago, así:
+// a. Para prepago por compra de equipos entre $100000 y $900000, el descuento es del 10% y el 20% de
+// descuento, superiores a $1000000.
+
+// b. En postpago si el plan cuesta menos de $500.000, el descuento es de 15%, si está entre $500.000 y 
+// 1000000 el descuento es del 20% y para valores superiores es del 25%.
+
+// c. Dada la compra de un plan, identificar el descuento realizado y el valor neto de la compra.
+
+
+// 4.Dado como dato el sueldo de un trabajador, aplíquele un aumento del 15% si su sueldo es inferior a 
+// $1000000. Imprima en este caso el nuevo sueldo del trabajador.
+
+// 5. Elaborar un programa, para calcular el salario semanal de un empleado al que se le paga 40000 pesos 
+// por hora si estas no superan las 35 horas. Cada hora por encima de 35 se considera extras y se paga a 
+// 5000 pesos
+
+// 6. Un primer automóvil se mueve de la ciudad A a la ciudad B, otro automóvil se mueve de la ciudad B a 
+// la ciudad A, ambos se desplazan con movimiento rectilíneo uniforme. Si se conocen sus velocidades y la 
+// distancia entre la ciudad A y la ciudad B, calcular el tiempo al cabo del cual los automóviles se
+// encuentran.
+
+
+// 7. Escriba un programa que usa while para la sumar los números enteros de 1 a 100.
+
+
+// 8. Escriba un programa para determinar si un entero es par o impar
